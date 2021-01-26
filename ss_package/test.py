b81f0ad3dfb5f49b8f97a5c51cd84eefaf045f9c
Njeri <njeri.gachago@gmail.com>
2021-01-26 22:44:52 +0900

    updated preprocessing package

--- ss_package/test.py
+++
@@ -1,9 +0,0 @@
-# Test could include:
-
-# Data loading & processing
-# checking that df is right shape:
-    # after loading data (20000, 25)
-    # after processing columns (19997, 16)
-    # after processing text (19997, 16)
-    # after feature engineering (19995, 17)
-# checking that there are no null values
